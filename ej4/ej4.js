3676ff0966093e1539ce23f5c84d78d6c8a03456
Robyn <scuadra02@gmail.com>
2024-04-15 02:45:00 -0400

    TODO

---
+++ ej4/ej4.js
@@ -0,0 +1,100 @@
+
+
+
+const addButton = document.querySelector(".add-button");
+const addModal = document.querySelector(".add-modal");
+const addModalButton = addModal.querySelector(".add-modal_submit")
+const addModalForm = addModal.querySelector(".add-modal_form")
+
+addButton.addEventListener("click", () => addModal.show());
+
+const todoContainer = document.querySelector(".todo-container");
+
+function addTodo(nameValue) {
+  const todoTemplate = document.querySelector("#todo-template").content;
+  const todoElement = todoTemplate.querySelector(".todo_item").cloneNode(true);
+  const deleteButton = todoElement.querySelector(".delete-button");
+  todoElement.querySelector(".todo_name").textContent = nameValue;
+  deleteButton.addEventListener('click', (e) => {
+    e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+    console.log("delete")
+  })
+  todoContainer.append(todoElement);
+  saveToLocalStorage();
+
+}
+
+  addModalForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const name = addModal.querySelector(".add-modal_name");
+    addTodo(name.value);
+    name.value = "";
+   
+  })
+
+  const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add("form__input_type_error");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add("form__input-error_active");
+  };
+  
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove("form__input_type_error");
+    errorElement.classList.remove("form__input-error_active");
+    errorElement.textContent = "";
+  };
+  
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.value) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+  
+  const hasInvalidInput = (inputElement) => {
+    if (inputElement.value) {
+      return false
+    } else {
+      return true
+    }
+  };
+  
+  const toggleButtonState = (inputElement, buttonElement) => {
+    console.log(hasInvalidInput(inputElement));
+    if (hasInvalidInput(inputElement)) {
+      buttonElement.classList.add("button_inactive");
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classList.remove("button_inactive");
+      buttonElement.disabled = false;
+    }
+  };
+  
+  const setEventListeners = (formElement) => {
+    const inputElement = formElement.querySelector(".form__input");
+    const buttonElement = formElement.querySelector(".form__submit");
+      toggleButtonState(inputElement, buttonElement);
+      inputElement.addEventListener("input", function () {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputElement, buttonElement);
+    });
+  };
+  
+  const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll(".form"));
+    formList.forEach((formElement) => {
+      formElement.addEventListener("submit", function (evt) {
+        evt.preventDefault();
+        addModal.hide()
+      });
+    console.log(formList)
+      formList.forEach((fieldset) => {
+        setEventListeners(fieldset);
+      });
+    });
+  };
+  
+enableValidation();
